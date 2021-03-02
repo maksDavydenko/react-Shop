7af232fd072150368d0c0c2bd42884f096f803dd
maksym davydenko <Maksym_Davydenko@epam.com>
2021-03-02 21:00:49 +0200

    add count basket

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,8 +7,6 @@ import { Context } from '../../contex';
 function Header() {
     const { staffInBasket } = useContext(Context);
 
-
-    console.log("staffInBasket.length", staffInBasket.length)
     return (
         <header className="header">
             <NavLink to="/">
@@ -16,7 +14,7 @@ function Header() {
                 </NavLink>
             <NavLink to="/basket">
                 <div className="basket">
-                    {staffInBasket.length > 0 ? <div className="basket__count">{staffInBasket.length}</div> : null}
+                    {staffInBasket.length > 0 ? <div className="basket__count">{[...new Set(staffInBasket)].length}</div> : null}
                     <img src="https://cdn1.iconfinder.com/data/icons/ecommerce-1-9/48/2-512.png" alt="basket" width="25" height="25" />
                 </div>
             </NavLink>
